571fb8c68842c225e3013fbec87fead1544a3b5a
DanaWentBananas <80426346+DanaWentBananas@users.noreply.github.com>
2021-04-01 21:05:20 +0300

    hellooo

---
+++ main.js
@@ -0,0 +1,33 @@
+
+// _____________DECLARING VARIABLES_____________
+
+const navbtn=document.querySelector('.nav-btn');
+const menu=document.querySelector('.menu-container');
+
+
+// _____________SLIDING NAVINGATION_____________
+
+// button
+
+navbtn.click= ()=>{
+  menu.style.clipPath="circle(0%)";
+}
+
+
+// navbtn.addEventListener('click', ()=>{
+//
+//  menu.style
+
+   // slides of content
+  // navContent.forEach((link, index)=>{
+  //   if(link.style.animation){
+  //     link.style.animation='';
+  //   }
+  //   else{
+  //   link.style.animation=`navContentslide 0.5s ease forwards ${index/7+0.3}s`;
+  //   }
+//
+// });
+
+
+});
