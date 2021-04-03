58b1de7d598964e060814e9b32e71f5c57e238c2
DanaWentBananas <80426346+DanaWentBananas@users.noreply.github.com>
2021-04-03 22:18:54 +0300

    here we go agai

--- main.js
+++ main.js
@@ -7,27 +7,6 @@ const menu=document.querySelector('.menu-container');
 
 // _____________SLIDING NAVINGATION_____________
 
-// button
-
 navbtn.click= ()=>{
   menu.style.clipPath="circle(0%)";
 }
-
-
-// navbtn.addEventListener('click', ()=>{
-//
-//  menu.style
-
-   // slides of content
-  // navContent.forEach((link, index)=>{
-  //   if(link.style.animation){
-  //     link.style.animation='';
-  //   }
-  //   else{
-  //   link.style.animation=`navContentslide 0.5s ease forwards ${index/7+0.3}s`;
-  //   }
-//
-// });
-
-
-});
